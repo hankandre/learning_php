e960ba11fd805b27225e222c6972afd8357d4143
Hank Andre <hankandre@gmail.com>
2016-09-05 15:43:51 -0500

    Experimenting with variables in PHP

---
+++ numbers.php
@@ -0,0 +1,29 @@
+<?php 
+/* 
+  
+INTEGERS
+
+  $num_one = 1;
+  $num_two = 2;
+  $num_three = 3;
+  var_dump( $num_one);
+  var_dump(1);
+  var_dump("1");
+
+FLOATING POINT
+  $distance_home = 5.6;
+  $distance_work = 3.2;
+  var_dump($distance_home + $distance_work + $num_three + .2);
+
+ARITHMETIC OPERATORS
+$a = 5; // Set the variables
+$b = 3; 
+
+-$a === -5; //Negation of a variable.
+$a - $b = 2 //Subtraction of two variables.
+$a + $b = 8 //Addition of two variables.
+$a * $b = 15 //Multiplication of two variables.
+ 
+*/
+
+?>
